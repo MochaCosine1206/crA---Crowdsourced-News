c3f95d60a14f11f3a5b462024f1503a8acc1392b
StevenXPS13 <steven.parrott@gmail.com>
2019-04-06 13:51:57 -0700

    commented out keys file ref

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require("./keys")
+// const keys = require("./keys")
 const User = require("../models/User");
 
 passport.serializeUser((user, done) => {
