3e982e6c77de3d1d6ab96036203a45355aa75e24
StevenXPS13 <steven.parrott@gmail.com>
2019-04-12 13:10:22 -0700

    changed button for heroku

--- client/src/components/GoogleAuth/index.js
+++ client/src/components/GoogleAuth/index.js
@@ -19,16 +19,16 @@ class PostDetailPage extends Component {
   render() {
     return (
       <div>
-        {/* <a href="http://localhost:3001/auth/google" className="button rounded-pill text-center"> */}
-          <button onClick={this.getGoogleAuth} className="button">
+        <a href="/auth/google" className="button rounded-pill text-center">
+          {/* <button onClick={this.getGoogleAuth} className="button"> */}
 
           <div >
             <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="..." />
             <span className="button-label">Sign in with Google</span>
           </div>
-          </button>
-        {/* </a> */}
-      </div>
+          {/* </button> */}
+        </a>
+      {/* </div> */}
     );
   }
 
