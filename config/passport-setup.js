040d8718f0fd1cabe48f2187204f2f5011f205e9
StevenXPS13 <steven.parrott@gmail.com>
2019-04-12 22:18:50 -0700

    removed manifest,favicon, changed callbackurl cnfg

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -21,7 +21,7 @@ passport.use(
         clientID: process.env.GOOGLE_clientID,
         // clientSecret: keys.google.clientSecret,
         clientSecret: process.env.GOOGLE_clientSecret,
-        callbackURL: '/auth/google/callback',
+        callbackURL: 'https://localhost:3001/auth/google/callback',
     }, (accessToken, refreshToken, profile, done) => {
         //passport callback function
         //check if user already exists
